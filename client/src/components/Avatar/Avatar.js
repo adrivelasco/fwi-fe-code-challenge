4b57e739fbe8f2aed8c3a364374f3cddd5ada94e
Adrian Velasco <adrivelasco96@hotmail.com>
2019-04-30 01:42:51 -0300

    Reusable components

--- client/src/components/Avatar/Avatar.js
+++ client/src/components/Avatar/Avatar.js
@@ -3,13 +3,16 @@ import PropTypes from 'prop-types';
 
 import './Avatar.scss';
 
-const Avatar = ({ src, children }) => (
-  <span className="avatar">{children || <img src={src} alt="" />}</span>
+const Avatar = ({ src, children, style }) => (
+  <span style={style} className="avatar">
+    {children || <img src={src} alt="" />}
+  </span>
 );
 
 Avatar.propTypes = {
   src: PropTypes.string,
   children: PropTypes.node,
+  style: PropTypes.object,
 };
 
 export default Avatar;
--- client/src/components/Button/Button.js
+++ client/src/components/Button/Button.js
@@ -1,19 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import cs from 'classnames';
 
 import './Button.scss';
 
-const Button = ({ onClick, children }) => {
+const types = {
+  theme: ['light', 'dark'],
+  variant: ['outlined', 'flat', 'contained'],
+  color: ['default', 'primary', 'success', 'error'],
+};
+
+const Button = props => {
+  const { onClick, children } = props;
+  const btnClasses = {};
+
+  for (const type in types) {
+    const values = types[type];
+    if (values) {
+      values.forEach(val => {
+        btnClasses[`btn--${val}`] = props[type] === val;
+      });
+    }
+  }
+
   return (
-    <button className="btn-base" onClick={onClick}>
+    <button className={cs({ btn: true, ...btnClasses })} onClick={onClick}>
       {children}
     </button>
   );
 };
 
 Button.propTypes = {
+  variant: PropTypes.oneOf(types.variant),
+  color: PropTypes.oneOf(types.color),
   onClick: PropTypes.func,
   children: PropTypes.node,
+};
+
+Button.defaultProps = {
+  theme: 'light',
+  variant: 'flat',
+  color: 'default',
 };
 
 export default Button;
